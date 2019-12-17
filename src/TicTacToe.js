d79b687c70e38980828d06c1c23c41c7763128c3
Filipe Rocha <fsrbillie@gmail.com>
2019-12-17 18:07:24 -0300

    add comentarios

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,13 +1,17 @@
 import React,{ useState} from 'react';
 
 import './TicTacToe.css';
+ //video parado 20:52
+ //https://www.youtube.com/watch?v=kFXDcaUcOto
+
 
 function TicTacToe() {
-
+  //cria classe, campos para o jogo
   const empityBoard = Array(9).fill("");
   const[board,setBoard]=useState(empityBoard);
+  //criar jogadores por default O
   const[currentPlayer,setcurrentPlayer]=useState("O");
-
+  // funcção que aou clicar verifica o jogador e seta no campo
   const handlecellclick = (index)=>{
     setBoard(board.map((item, itemIndex)=> itemIndex === index ? currentPlayer : item));
     setcurrentPlayer(currentPlayer === "X" ? "O" :"X");
@@ -18,10 +22,14 @@ function TicTacToe() {
       <h1 className="title">Jogo da Velha</h1>
 
       <div className="board">
+
         {board.map((item,index)=>(
+          // reenderiza campos
           <div
+          // cada div recebe um valor
             key={index}
             className={`cell ${item}`}
+          // cria valor para item
             onClick={()=> handlecellclick(index)}
           >
             {item}
